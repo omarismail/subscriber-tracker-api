f57aafe1db4bfc1c6401dc0a7d9555ecdcebf2fe
Omar Ismail <oismail@digitalocean.com>
2020-07-28 15:13:29 -0400

    edit alrge tes

--- large-text.sh
+++ large-text.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-while true
-do
-  name=$(uuidgen)
-  dd if=/dev/zero of="${name}.txt" count=1000 bs=1000000
-done
+while true; do name=$(openssl rand -hex 16); dd if=/dev/zero of="${name}.txt" count=1000 bs=1000000; done;
